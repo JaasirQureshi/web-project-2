b59a01ba2e7b80d3af184d1d23d36fde9c31a380
jaasir qureshi <jaasir.qureshi@hotmail.com>
2019-11-20 20:41:14 -0600

    intial commit

---
+++ Project 2/Properties/JavaScript.js
@@ -0,0 +1,16 @@
+﻿function start()
+{
+    document.getElementById("mount");
+    document.getElementById("jungle");
+    document.getElementById("ocean");
+    document.getElementById("desert");
+    document.getElementById("bleach");
+    document.getElementById("home").style.backgroundImage = "bg5.jpg";
+}
+
+function homeImg()
+{
+    document.body.style.backgroundImage = "('bg5.jpg')";
+}   
+
+window.addEventListener("load", start, false);
